82cc0d58d5b7d9aed8253f923b0908b574555344
nyakuro <nyakuro0714@gmail.com>
2017-05-10 20:16:21 +0900

    [remove] unused

--- src/OAuth/OAuth2/Service/FriendsNico.php
+++ src/OAuth/OAuth2/Service/FriendsNico.php
@@ -2,14 +2,6 @@
 
 namespace OAuth\OAuth2\Service;
 
-use OAuth\OAuth2\Token\StdOAuth2Token;
-use OAuth\Common\Http\Exception\TokenResponseException;
-use OAuth\Common\Http\Uri\Uri;
-use OAuth\Common\Consumer\CredentialsInterface;
-use OAuth\Common\Http\Client\ClientInterface;
-use OAuth\Common\Storage\TokenStorageInterface;
-use OAuth\Common\Http\Uri\UriInterface;
-
 class FriendsNico extends Mastodon
 {
     protected $host = 'https://friends.nico';
